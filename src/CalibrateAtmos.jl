978fdba6a2414b5017165232531b2bfea64b80e2
akshaysridhar <asridhar@caltech.edu>
2024-02-22 10:40:18 -0800

    modified:   Project.toml 	modified:   experiments/Manifest.toml 	modified:   experiments/Project.toml 	new file:   experiments/amip_coupled/coupler_config.yml 	new file:   experiments/amip_coupled/ekp_config.yml 	new file:   experiments/amip_coupled/generate_observations.sbatch 	new file:   experiments/amip_coupled/model_run.sbatch 	modified:   src/CalibrateAtmos.jl 	new file:   src/coupler_interface.jl 	new file:   test/test_coupler_config.jl

--- src/CalibrateAtmos.jl
+++ src/CalibrateAtmos.jl
@@ -1,6 +1,10 @@
 module CalibrateAtmos
 
+using SciMLBase
+
 include("ekp_interface.jl")
 include("atmos_interface.jl")
+include("coupler_interface.jl")
+#include("/Users/akshaysridhar/Research/Codes/ClimaCoupler.jl/experiments/AMIP/coupler_driver.jl")
 
 end # module CalibrateAtmos
