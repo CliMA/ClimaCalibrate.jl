9a3b4232d0fabbf24dd81eb37a281a21206c206c
nefrathenrici <nat.henrici@gmail.com>
2024-02-28 15:43:58 -0800

    Set test to use Random.seed

--- test/test_init.jl
+++ test/test_init.jl
@@ -12,21 +12,20 @@ param_names = ["one", "two"]
 config = Dict(
     "output_dir" => output_dir,
     "prior_path" => prior_path,
-    "parameter_names" => param_names,
-    "ensemble_size" => 10,
+    "ensemble_size" => 1,
 )
 Γ = 0.1 * I
 y = zeros(Float64, 1)
 
-CalibrateAtmos.initialize("test"; config, Γ, y)
+CalibrateAtmos.initialize("test"; config, Γ, y, rng_seed = 4444)
 
 override_file =
     joinpath(output_dir, "iteration_000", "member_001", "parameters.toml")
 td = CP.create_toml_dict(FT; override_file)
-params = (; CP.get_parameter_values(td, param_names)...)
+params = CP.get_parameter_values(td, param_names)
 
 @testset "Initialized parameter values" begin
     # This checks for random seed as well
-    @test params.one == 3.520967031258908
-    @test params.two == 4.726862935348823
+    @test params.one == 2.7212695972038583
+    @test params.two == 4.891199610995353
 end
