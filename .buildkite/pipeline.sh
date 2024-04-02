33deee2caeda63b6d96e28b2dcb1449cfa106044
nefrathenrici <nat.henrici@gmail.com>
2024-04-02 10:18:10 -0700

    don't build calibrateatmos first

--- .buildkite/pipeline.sh
+++ .buildkite/pipeline.sh
@@ -23,9 +23,8 @@ steps:
   - label: Initialize
     key: init
     command: |
-      julia --project=$exp_dir -e '
-        import Pkg; Pkg.build("CalibrateAtmos")
-        Pkg.instantiate(;verbose=true)'
+      julia --project -e 'import Pkg; Pkg.instantiate(;verbose=true)'
+      julia --project=$exp_dir -e 'import Pkg; Pkg.instantiate(;verbose=true)'
 EOM
 
 if [ "$generate_data" == "true" ] ; then
