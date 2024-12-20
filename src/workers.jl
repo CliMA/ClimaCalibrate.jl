a62386e71b52f5706613e0bb0ef3fa9f8505f4e1
nefrathenrici <nat.henrici@gmail.com>
2024-12-19 16:16:57 -0800

    Update documentation, need to update atmos guide

--- src/workers.jl
+++ src/workers.jl
@@ -45,6 +45,16 @@ worker_cookie() = begin
 end
 worker_arg() = `--worker=$(worker_cookie())`
 
+"""
+    SlurmManager(ntasks=get(ENV, "SLURM_NTASKS", 1))
+
+The ClusterManager for Slurm clusters, taking in the number of tasks to request with `srun`.
+To execute the `srun` command, run `addprocs(SlurmManager(ntasks))`
+Keyword arguments can be passed to `srun`: `addprocs(SlurmManager(ntasks), gpus_per_task=1)`
+By default the workers will inherit the running Julia environment.
+To run a calibration, call `calibrate(WorkerBackend, ...)`
+To run functions on a worker, call `remotecall(func, worker_id, args...)`
+"""
 struct SlurmManager <: ClusterManager
     ntasks::Integer
 
