5e4b4b3e6e94945dc516d1f200dbf92c75784b7d
Kevin Phan <98072684+ph-kev@users.noreply.github.com>
2025-06-16 09:56:17 -0700

    temp

--- docs/make.jl
+++ docs/make.jl
@@ -2,23 +2,23 @@ using Documenter
 using Documenter: doctest
 using ClimaCalibrate
 import ClimaAnalysis # needed to load ClimaAnalysis extension
-using Base.CoreLogging
-using DocumenterCitations
-import Literate
+# using Base.CoreLogging
+# using DocumenterCitations
+# import Literate
 
-disable_logging(Base.CoreLogging.Info) # Hide doctest's `@info` printing
-bib = CitationBibliography(joinpath(@__DIR__, "bibliography.bib"))
+# disable_logging(Base.CoreLogging.Info) # Hide doctest's `@info` printing
+# bib = CitationBibliography(joinpath(@__DIR__, "bibliography.bib"))
 
-doctest(ClimaCalibrate; plugins = [bib])
-disable_logging(Base.CoreLogging.BelowMinLevel) # Re-enable all logging
+# doctest(ClimaCalibrate; plugins = [bib])
+# disable_logging(Base.CoreLogging.BelowMinLevel) # Re-enable all logging
 
-Literate.markdown(
-    joinpath(@__DIR__, "literate_example.jl"),
-    joinpath(@__DIR__, "src"),
-)
+# Literate.markdown(
+#     joinpath(@__DIR__, "literate_example.jl"),
+#     joinpath(@__DIR__, "src"),
+# )
 
 makedocs(
-    plugins = [bib],
+    # plugins = [bib],
     modules = [
         ClimaCalibrate,
         Base.get_extension(ClimaCalibrate, :ClimaAnalysisExt),
@@ -34,7 +34,7 @@ makedocs(
     pages = [
         "Home" => "index.md",
         "Getting Started" => "quickstart.md",
-        "Distributed Calibration Tutorial" => "literate_example.md",
+        # "Distributed Calibration Tutorial" => "literate_example.md",
         "Backends" => "backends.md",
         "Observations" => "observations.md",
         "Observation Recipes" => "observation_recipe.md",
