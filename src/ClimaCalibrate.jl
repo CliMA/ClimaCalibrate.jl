e72108be6ffc4987b7f5c10429992e5b397cec4d
ph-kev <98072684+ph-kev@users.noreply.github.com>
2026-09-11 11:39:05 -0700

    fixup! Add ObservationRecipe.reconstruct_residual

--- src/ClimaCalibrate.jl
+++ src/ClimaCalibrate.jl
@@ -67,6 +67,7 @@ const _CLIMAANALYSIS_STUBS = (
     ObservationRecipe.reconstruct_g_mean_final,
     ObservationRecipe.reconstruct_diag_cov,
     ObservationRecipe.reconstruct_vars,
+    ObservationRecipe.reconstruct_residual,
     EnsembleBuilder.GEnsembleBuilder,
     EnsembleBuilder.fill_g_ens_col!,
     EnsembleBuilder.is_complete,
